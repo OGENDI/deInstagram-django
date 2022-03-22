9cbc85fcdf9f4cc0b3a15d79aa9513fee601d838
OGENDI <demokonge17@gmail.com>
2022-03-22 13:14:58 +0300

    Initial project directory creation

---
+++ deInstagram/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for deInstagram project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deInstagram.settings')
+
+application = get_wsgi_application()
